54b56fcee059ee4c2ae06b8df492b943a4163aa8
pbauer <pbauer@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-09-19 09:23:43 +0000

    move enl_ubscribers_view to a browser view

---
+++ Products/EasyNewsletter/browser/subscribers.py
@@ -0,0 +1,40 @@
+from zope.interface import implements, Interface
+
+from Products.Five import BrowserView
+from Products.CMFCore.utils import getToolByName
+
+
+class IEnl_Subscribers_View(Interface):
+    """
+    Enl_Subscribers_View interface
+    """
+
+    def test():
+        """ test method"""
+
+
+class Enl_Subscribers_View(BrowserView):
+    """
+    Enl_Subscribers_View browser view
+    """
+    implements(IEnl_Subscribers_View)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    @property
+    def portal_catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+
+    @property
+    def portal(self):
+        return getToolByName(self.context, 'portal_url').getPortalObject()
+
+    def test(self):
+        """
+        test method
+        """
+        dummy = (u'a dummy string')
+
+        return {'dummy': dummy}
