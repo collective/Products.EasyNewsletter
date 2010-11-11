48c0be61eb7f064830cd62de80718e61c6525fb5
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-11 09:46:06 +0000

    added BeautifulSoup

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='Products.EasyNewsletter',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'BeautifulSoup',
           'Products.TemplateFields',
           'inqbus.plone.fastmemberproperties',
       ],
