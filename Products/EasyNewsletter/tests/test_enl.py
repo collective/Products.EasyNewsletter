5e415c26a502d1cc3b9a3dbfb9b1e0e04233d4e6
ajung <ajung@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-11-21 15:57:00 +0000

    more tests

--- Products/EasyNewsletter/tests/test_enl.py
+++ Products/EasyNewsletter/tests/test_enl.py
@@ -9,11 +9,14 @@ class EasyNewsletterTests(PloneTestCase):
 
     def afterSetUp(self):
         self.portal.portal_membership.addMember('admin', 'secret', ('Manager',), None)
-             
-    def testInstallation(self):
+
+    def _makeOne(self):
         self.login('admin')
         self.portal.invokeFactory('EasyNewsletter', id='newsletter')
-        enl = self.portal['newsletter']
+        return self.portal['newsletter']
+             
+    def testInstallation(self):
+        enl = self._makeOne()
         enl.invokeFactory('ENLIssue', id='issue')
         issue = enl['issue']
         enl.invokeFactory('ENLSubscriber', id='heino')
@@ -25,6 +28,16 @@ class EasyNewsletterTests(PloneTestCase):
         self.assertEqual('email' in indexes, True)
         self.assertEqual('fullname' in indexes, True)
         self.assertEqual('organization' in indexes, True)
+
+    def testDefaultView(self):
+        self.login('admin')
+        enl = self._makeOne()
+        html = enl.view()
+
+    def testSubscribertView(self):
+        self.login('admin')
+        enl = self._makeOne()
+        html = enl.restrictedTraverse('@@enl_subscribers_view')
 
 def test_suite():
     suite = unittest.TestSuite()
