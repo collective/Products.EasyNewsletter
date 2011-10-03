a587662f8dba7c37fc3a44051fbecd033a71c95f
regisrouet <regisrouet@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-03 11:13:46 +0000

    don't try to traverse absolute href links, don't try to embed images

--- Products/EasyNewsletter/utils/ENLHTMLParser.py
+++ Products/EasyNewsletter/utils/ENLHTMLParser.py
@@ -4,6 +4,9 @@ import urllib
 
 from Products.CMFCore.utils import getToolByName
 
+import logging
+logger = logging.getLogger("Products.EasyNewsletter.HTMLParser")
+
 
 class ENLHTMLParser(HTMLParser.HTMLParser):
     """A simple parser which exchange relative URLs with absolute ones"""
@@ -14,6 +17,10 @@ class ENLHTMLParser(HTMLParser.HTMLParser):
         self.html = ""
         self.image_urls = []
         self.image_number = 0
+        plone_utils = getToolByName(self.context, 'plone_utils')
+        self.encoding = plone_utils.getSiteEncoding()
+        # portal_url could also be taken from a newsletter parameter for people having public/private url
+        self.portal_url = getToolByName(context, "portal_url")()
 
         HTMLParser.HTMLParser.__init__(self)
 
@@ -25,26 +32,42 @@ class ENLHTMLParser(HTMLParser.HTMLParser):
 
         for attr in attrs:
             if attr[0] == "href":
-                try:
-                    # split anchor from url
-                    baseurl, anchor = urlparse.urldefrag(attr[1])
-                    o = self.context.restrictedTraverse(urllib.unquote(baseurl))
-                    if getattr(o, 'absolute_url', None):
-                        url = o.absolute_url()
-                    else:
-                        # maybe we got a view instead of an traversal object:
-                        if getattr(o, 'context', None):
-                            url = o.context.absolute_url()
-                        else:
-                            url = attr[1]
-                    if anchor:
-                        url = '#' + anchor
-                except:
+                url_tuple = urlparse.urlsplit(attr[1])
+                if url_tuple.scheme != '':
                     url = attr[1]
+                elif url_tuple.path.startswith('/'):
+                    url = '%s%s' % (self.portal_url, attr[1])
+                elif url_tuple.path.startswith('resolveuid/'):
+                    # consider resolveuid url as absolute
+                    # on sept 2011, TinyMCE create link ref as "resolveuid/..." 
+                    url = '%s/%s' % (self.portal_url, attr[1])
+                elif 'resolveuid/' in url_tuple.path:
+                    # on sept 2011, by default, TinyMCE convert user inserted absolute link to relative ones, ex; ../../../my_abs_url
+                    # this could help if this hasn't been applied : http://stackoverflow.com/questions/5196205/disable-tinymce-url-conversions
+                    # untested 
+                    url = '%s/%s' % (self.portal_url, attr[1][attr[1].find('resolveuid/'):])
+                elif attr[1].startswith('#'):
+                    url = attr[1]
+                else:
+                    logger.info("href main branch code")
+                    try:
+                        # split anchor from url
+                        baseurl, anchor = urlparse.urldefrag(attr[1])
+                        o = self.context.restrictedTraverse(urllib.unquote(baseurl))
+                        if getattr(o, 'absolute_url', None):
+                            url = o.absolute_url()
+                        else:
+                            # maybe we got a view instead of an traversal object:
+                            if getattr(o, 'context', None):
+                                url = o.context.absolute_url()
+                            else:
+                                url = attr[1]
+                        if anchor:
+                            url = '#' + anchor
+                    except:
+                        url = attr[1]
                 if isinstance(url, unicode):
-                    plone_utils = getToolByName(self.context, 'plone_utils')
-                    encoding = plone_utils.getSiteEncoding()
-                    url = url.encode(encoding)
+                    url = url.encode(self.encoding)
                 self.html += ' href="%s"' % url
             else:
                 self.html += ' %s="%s"' % (attr)
@@ -79,14 +102,26 @@ class ENLHTMLParser(HTMLParser.HTMLParser):
         self.html += "<%s" % tag
         for attr in attrs:
             if attr[0] == "src":
-                image_url = urlparse.urlparse(attr[1])
-                if 'http' in attr[1]:
+                url_tuple = urlparse.urlsplit(attr[1])
+                url = None
+                if url_tuple.scheme != '':
                     url = attr[1]
-                    self.html += ' src="%s"' % url
+                elif url_tuple.path.startswith('/'):
+                    url = '%s%s' % (self.portal_url, attr[1])
+                elif url_tuple.path.startswith('resolveuid/'):
+                    # consider resolveuid url as absolute
+                    # on sept 2011, TinyMCE create img src as "resolveuid/..." 
+                    url = '%s/%s' % (self.portal_url, attr[1])
+                elif 'resolveuid/' in url_tuple.path:
+                    # see href
+                    url = '%s/%s' % (self.portal_url, attr[1][attr[1].find('resolveuid/'):])
                 else:
+                    logger.info("src main branch code for embedding image")
                     self.html += ' src="cid:image_%s"' % self.image_number
                     self.image_number += 1
                     self.image_urls.append(attr[1])
+                if url is not None:
+                    self.html += ' src="%s"' % url
             else:
                 self.html += ' %s="%s"' % (attr)
 
