3324c20a4e10c234580aedfe131760b7bad2d017
Maik Derstappen <maik.derstappen@inqbus.de>
2012-07-05 20:19:18 +0200

    cleanup Newsletter UI: move action for archive, drafts and subscribers into a viewlet, move criteria and subfolder actions into object_buttons

---
+++ Products/EasyNewsletter/browser/viewlets.py
@@ -0,0 +1,9 @@
+from plone.app.layout.viewlets.common import ViewletBase
+
+class ENLActionsViewlet(ViewletBase):
+
+  def update(self):
+    """
+    """
+    self.id = self.context.id
+    self.enl_url = self.context.absolute_url()
