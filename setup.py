1dab6be589e9fa5f25aedd4799bb027b654be8b5
hvelarde <hector.velarde@gmail.com>
2013-11-01 16:24:22 -0200

    Back to development: 2.6.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.6.12'
+version = '2.6.13.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
