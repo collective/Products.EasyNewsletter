3e25aeeb629098d6696e6fb31213b0c6ac344edb
MrTango <md@derico.de>
2015-05-12 11:30:23 +0200

    add upgrade step after code of ths step has changed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '3.0a1.dev0'
+version = '3.0a2.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
