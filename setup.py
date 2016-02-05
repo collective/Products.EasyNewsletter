4f37745c1624547c94da120cfa49b45f4c87fdd3
Peter Holzer <hpeter@agitator.com>
2016-02-05 16:15:39 +0100

    dev version shift

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '3.0a3.dev3'
+version = '3.0a3.dev4'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
