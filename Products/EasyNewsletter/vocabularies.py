7266c09efb7a428993d8fc7d242d84ce19e0b6f1
MrTango <md@derico.de>
2017-04-07 22:39:07 +0300

    pep8 fixes

--- Products/EasyNewsletter/vocabularies.py
+++ Products/EasyNewsletter/vocabularies.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from Products.EasyNewsletter.interfaces import IEasyNewsletter
