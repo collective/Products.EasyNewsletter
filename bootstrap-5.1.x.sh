18bf40f14d8bb1175d7b36763b03116e7b323ab5
MrTango <md@derico.de>
2017-04-10 15:07:45 +0300

    fix travis setup for Plone5.1

--- bootstrap-5.1.x.sh
+++ bootstrap-5.1.x.sh
@@ -4,5 +4,5 @@
 ln -fs plone-5.1.x.cfg buildout.cfg
 rm -r ./lib ./include ./local ./bin
 virtualenv --clear .
-./bin/pip install --upgrade pip setuptools zc.buildout
+./bin/pip install --upgrade pip setuptools==34.3.0 zc.buildout==2.8.0
 ./bin/buildout
