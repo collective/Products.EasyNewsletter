37f32421f36f8d54052195fe5450bc80c01d80a6
MrTango <md@derico.de>
2017-04-23 12:20:28 +0300

    fix plone 4 buildout

--- bootstrap-4.3.x.sh
+++ bootstrap-4.3.x.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 # see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
 ln -fs plone-4.3.x.cfg buildout.cfg
-virtualenv --clear .
-./bin/pip install --upgrade pip setuptools zc.buildout
-./bin/buildout 
+./bootstrap-base.sh
