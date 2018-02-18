34074fc3aae81c779de3f591588a95c8b7617b2d
MrTango2 <md@derico.de>
2018-02-18 22:32:43 +0200

    Back to development: 3.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '3.0.6'
+version = '3.0.7.dev0'
 long_description = (
     open('README.rst').read() + '\n' +
     open('CHANGES.rst').read()
