545551b05f01d7288dcf28a44d15f3eb0c671bb5
MrTango <md@derico.de>
2019-08-13 18:15:33 +0300

    cleanup config files

--- bootstrap-base.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# see https://community.plone.org/t/not-using-bootstrap-py-as-default/620
-rm -r ./lib ./include ./local ./bin
-virtualenv --clear .
-./bin/pip install --upgrade -r requirements.txt
