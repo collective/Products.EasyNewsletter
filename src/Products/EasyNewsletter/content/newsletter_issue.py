59b54c345ac086992b035b4d0df0c8d4c00aed02
MrTango <md@derico.de>
2019-08-15 17:28:02 +0300

    Finish Newsletter Issue schema

--- src/Products/EasyNewsletter/content/newsletter_issue.py
+++ src/Products/EasyNewsletter/content/newsletter_issue.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from .newsletter import get_content_aggregation_source_base_path
 from .newsletter import INewsletter
-from Acquisition import aq_inner
-from Acquisition import aq_parent
 from plone import api
 from plone import schema
+from plone.app import textfield
 from plone.app import vocabularies
 from plone.app.z3cform.widget import SingleCheckBoxBoolFieldWidget
 from plone.autoform import directives
@@ -26,9 +25,39 @@ def get_default_output_template(context):
         return context.output_template
 
 
+@provider(IContextAwareDefaultFactory)
+def get_default_prologue(context):
+    """ get prologue from parent Newsletter
+    """
+    if INewsletter.providedBy(context):
+        return context.default_prologue
+
+
+@provider(IContextAwareDefaultFactory)
+def get_default_epilogue(context):
+    """ get epilogue from parent Newsletter
+    """
+    if INewsletter.providedBy(context):
+        return context.default_epilogue
+
+
 class INewsletterIssue(model.Schema):
     """ Marker interface and Dexterity Python Schema for NewsletterIssue
     """
+
+    model.fieldset(
+        "customizations",
+        label=_(u"Customizations"),
+        fields=[
+            "default_prologue",
+            "default_epilogue",
+            "content_aggregation_source",
+            "exclude_all_subscribers",
+            "image",
+            "hide_image",
+            "output_template",
+        ],
+    )
 
     directives.widget(
         "content_aggregation_source",
@@ -78,6 +107,62 @@ class INewsletterIssue(model.Schema):
         defaultFactory=get_default_output_template,
         required=True,
     )
+
+    # Make sure to import: plone.app.textfield
+    default_prologue = textfield.RichText(
+        title=_(u"ENL_label_default_header", default=u"Prologue"),
+        description=_(
+            u"ENL_description_text_header",
+            default=u"The default prologue text. This is used as a default \
+                for new issues. You can use placeholders like\
+                {{SUBSCRIBER_SALUTATION}} and {{UNSUBSCRIBE}} here.",
+        ),
+        defaultFactory=get_default_prologue,
+        required=False,
+    )
+
+    # Make sure to import: plone.app.textfield
+    default_epilogue = textfield.RichText(
+        title=_(u"ENL_label_default_footer", default=u"Epilogue"),
+        description=_(
+            u"ENL_description_text_footer",
+            default=u"The default epilogue text. This is used as a default \
+                for new issues. You can use placeholders like\
+                {{SUBSCRIBER_SALUTATION}} and {{UNSUBSCRIBE}} here.",
+        ),
+        defaultFactory=get_default_epilogue,
+        required=False,
+    )
+
+    # Make sure you import:
+    # plone.app.z3cform.widget.SingleCheckBoxBoolFieldWidget
+    directives.widget(hide_image=SingleCheckBoxBoolFieldWidget)
+    hide_image = schema.Bool(
+        title=_(u"label_issueHideImage", default=u"Hide banner image."),
+        description=_(
+            u"enl_issue_help_hide_image",
+            default=u"If checked, the banner image defined on newsletter \
+                    or on this issue will not be used.",
+        ),
+        required=False,
+        default=False,
+        readonly=False,
+    )
+
+    # Make sure you import: plone.namedfile
+    image = namedfile.NamedBlobImage(
+        title=_(u"ENL_image_label", default=u"Banner image"),
+        description=_(
+            u"ENL_image_desc",
+            default=u"Banner image, you can include in the templates by"
+            + u"\n adding the {{banner}} placeholder into it."
+            + u" By default it should be 600x200 pixel.",
+        ),
+        required=False,
+    )
+
+    # directives.order_after(content_aggregation_source="IBasic.title")
+    # directives.order_after(output_template="IRichText.text")
 
 
 @implementer(INewsletterIssue)
