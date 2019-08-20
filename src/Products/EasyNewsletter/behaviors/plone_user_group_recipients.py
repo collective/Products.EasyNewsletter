67ecb0f9f0912ef69038da8e5f67cac9d92bea0c
MrTango <md@derico.de>
2019-08-20 19:44:35 +0300

    implement PloneUserGroupRecipients

--- src/Products/EasyNewsletter/behaviors/plone_user_group_recipients.py
+++ src/Products/EasyNewsletter/behaviors/plone_user_group_recipients.py
@@ -14,15 +14,54 @@ from zope.interface import provider
 class IPloneUserGroupRecipientsMarker(Interface):
     pass
 
+
 @provider(IFormFieldProvider)
 class IPloneUserGroupRecipients(model.Schema):
     """
     """
+    model.fieldset(
+        "recipients",
+        label=_(u"recipients"),
+        fields=[
+            "plone_receiver_members",
+            "plone_receiver_groups",
+        ],
+    )
 
-    project = schema.TextLine(
-        title=_(u'Project'),
-        description=_(u'Give in a project name'),
+    plone_receiver_members = schema.Set(
+        title=_(
+            u"EasyNewsletter_label_ploneReceiverMembers",
+            default=u"Plone Members to receive the newsletter",
+        ),
+        description=_(
+            u"EasyNewsletter_help_ploneReceiverMembers",
+            default=u"Choose Plone Members which should receive \
+                    the newsletter. Changing this setting does not affect \
+                    already existing issues.",
+        ),
+        value_type=schema.Choice(vocabulary=u"plone.app.vocabularies.PortalTypes"),
         required=False,
+        default=set(["Folder", "Image"]),
+        # defaultFactory=get_default_plone_receiver_members,
+        readonly=False,
+    )
+
+    plone_receiver_groups = schema.Set(
+        title=_(
+            u"EasyNewsletter_label_ploneReceiverGroups",
+            default=u"Plone Groups to receive the newsletter",
+        ),
+        description=_(
+            u"EasyNewsletter_help_ploneReceiverGroups",
+            default=u"Choose Plone Groups which should receive \
+                    the newsletter. Changing this setting does not affect \
+                    already existing issues.",
+        ),
+        value_type=schema.Choice(vocabulary=u"plone.app.vocabularies.PortalTypes"),
+        required=False,
+        default=set(["Folder", "Image"]),
+        # defaultFactory=get_default_plone_receiver_members,
+        readonly=False,
     )
 
 
@@ -33,11 +72,17 @@ class PloneUserGroupRecipients(object):
         self.context = context
 
     @property
-    def project(self):
-        if hasattr(self.context, 'project'):
-            return self.context.project
-        return None
+    def plone_receiver_members(self):
+        return self.context.plone_receiver_members
 
-    @project.setter
-    def project(self, value):
-        self.context.project = value
+    @plone_receiver_members.setter
+    def plone_receiver_members(self, value):
+        self.context.plone_receiver_members = value
+
+    @property
+    def plone_receiver_groups(self):
+        return self.context.plone_receiver_groups
+
+    @plone_receiver_groups.setter
+    def plone_receiver_groups(self, value):
+        self.context.plone_receiver_groups = value
