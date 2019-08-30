0f126b7c91822b01f2bd300853f709da917033e5
MrTango <md@derico.de>
2019-08-30 12:12:27 +0300

    "Add view: view"

---
+++ src/Products/EasyNewsletter/views/newsletter_issue.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from Products.EasyNewsletter import _
+from Products.Five.browser import BrowserView
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class NewsletterIssue(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('newsletter-issue.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        self.msg = _(u'A small message')
+        return self.index()
