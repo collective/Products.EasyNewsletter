a7ad848722f52704e56e1b804708657480164921
MrTango <md@derico.de>
2019-08-31 09:56:22 +0300

    "Add view: copy-as-draft"

---
+++ src/Products/EasyNewsletter/tests/test_view_copy_as_draft.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from Products.EasyNewsletter.testing import PRODUCTS_EASYNEWSLETTER_FUNCTIONAL_TESTING
+from Products.EasyNewsletter.testing import PRODUCTS_EASYNEWSLETTER_INTEGRATION_TESTING
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = PRODUCTS_EASYNEWSLETTER_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_copy_as_draft_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='copy-as-draft'
+        )
+        self.assertTrue(view.__name__ == 'copy-as-draft')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in copy-as-draft'
+        # )
+
+    def test_copy_as_draft_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='copy-as-draft'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = PRODUCTS_EASYNEWSLETTER_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
