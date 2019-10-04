c7b045580c8474242bd8205e0d4b7516976ebccc
MrTango <md@derico.de>
2019-10-04 15:24:27 +0200

    Preparing release 5.0.0a4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '5.0.0a4.dev0'
+version = '5.0.0a4'
 long_description = (
     open('README.rst').read() + '\n'
     + open('CHANGES.rst').read()
