bd75adc01c2eb285a197af5d5b1c1e8380acbbc8
MrTango <md@derico.de>
2019-10-07 18:54:32 +0200

    fix linting issues

--- src/Products/EasyNewsletter/tests/test_placeholder.py
+++ src/Products/EasyNewsletter/tests/test_placeholder.py
@@ -6,18 +6,8 @@ from plone.app.testing import login
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
-from plone.registry.interfaces import IRegistry
-from Products.CMFPlone.interfaces.controlpanel import IMailSchema
-from Products.CMFPlone.tests.utils import MockMailHost
-from Products.EasyNewsletter.interfaces import IBeforePersonalizationEvent
 from Products.EasyNewsletter.interfaces import IIssueDataFetcher
 from Products.EasyNewsletter.testing import PRODUCTS_EASYNEWSLETTER_FUNCTIONAL_TESTING
-from Products.EasyNewsletter.utils.mail import get_portal_mail_settings
-from Products.MailHost.interfaces import IMailHost
-from zope.component import getGlobalSiteManager
-from zope.component import getSiteManager
-from zope.component import getUtility
-from zope.component import provideHandler
 
 import unittest
 
