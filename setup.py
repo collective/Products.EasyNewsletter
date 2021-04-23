97d866294f206d42824ae816d33b6de18ba7188f
MrTango <md@derico.de>
2021-04-23 08:50:12 +0300

    Back to development: 5.0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '5.0.5'
+version = '5.0.6.dev0'
 long_description = (
     open('README.rst').read() + '\n'
     + open('CHANGES.rst').read()
